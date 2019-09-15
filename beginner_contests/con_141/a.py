843f777b2b91555e903ec0c756cafeb2cf842105
Shizuma <kirua515@gmail.com>
2019-09-15 21:10:47 +0900

    a問題 AC

---
+++ beginner_contests/con_141/a.py
@@ -0,0 +1,10 @@
+S = input()
+
+weather = ['Sunny', 'Cloudy', 'Rainy']
+
+if S == weather[0]:
+    print(weather[1])
+elif S == weather[1]:
+    print(weather[2])
+elif S == weather[2]:
+    print(weather[0])
