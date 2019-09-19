d5621fb65acbcc95c8bb7a17f95fb90897cd7f03
Shizuma <kirua515@gmail.com>
2019-09-19 18:13:17 +0900

    B問題 AC

--- beginner_contests/con_136/b.py
+++ beginner_contests/con_136/b.py
@@ -2,4 +2,21 @@ import sys
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N = int(input())
+
+digit = len(str(N))
+
+if digit == 1:
+    ans = N
+elif digit == 2:
+    ans = 9
+elif digit == 3:
+    ans = N - 99 + 9
+elif digit == 4:
+    ans = 909
+elif digit == 5:
+    ans = N - 9999 + 900 + 9
+elif digit == 6:
+    ans = 90000 + 900 + 9
+
+print(ans)
