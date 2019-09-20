cee415b63a54d40ced60d37244dfb1e5df2aadab
Shizuma <kirua515@gmail.com>
2019-09-21 05:26:19 +0900

    C問題 AC

--- beginner_contests/con_136/c.py
+++ beginner_contests/con_136/c.py
@@ -2,4 +2,14 @@ import sys
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N = int(input())
+
+H = list(map(int, input().split()))
+
+for i in range(N - 2, 0, -1):
+    if H[i] > H[i + 1]:
+        H[i] -= 1
+    if H[i] > H[i + 1]:
+        print('No')
+        sys.exit()
+print('Yes')
