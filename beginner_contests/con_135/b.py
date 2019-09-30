76cf1937c9b571361d900fcec664e2eb3cc5af46
Shizuma <kirua515@gmail.com>
2019-09-30 17:18:57 +0900

    B問題 AC

--- beginner_contests/con_135/b.py
+++ beginner_contests/con_135/b.py
@@ -2,4 +2,16 @@ import sys
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N = int(input())
+P = list(map(int, input().split()))
+
+miss_count = 0
+
+for i, p in enumerate(P, start=1):
+    if not i == p:
+        miss_count += 1
+
+    if miss_count > 2:
+        print('NO')
+        sys.exit()
+print('YES')
