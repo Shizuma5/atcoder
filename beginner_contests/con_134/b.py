d4759002be5b97db27b9d9c0d4fb41e16f29236e
Shizuma <kirua515@gmail.com>
2019-10-04 13:51:52 +0900

    B問題 AC

--- beginner_contests/con_134/b.py
+++ beginner_contests/con_134/b.py
@@ -1,5 +1,10 @@
+import math
 import sys
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N, D = map(int, input().split())
+
+watch_range = D * 2 + 1
+
+print(math.ceil(N / watch_range))
