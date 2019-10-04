35a10f696adb49801b763f35a0d115044cbfb6d8
Shizuma <kirua515@gmail.com>
2019-10-04 16:50:15 +0900

    D問題 AC（解答見た）

--- beginner_contests/con_134/d.py
+++ beginner_contests/con_134/d.py
@@ -1,5 +1,30 @@
+# https://note.mu/tanon_cp/n/nf77205916310
+
 import sys
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
+
+flag=[-1]*N #0～n-1番目の箱にボールを入れたかどうかの情報を入れる
+ans=[]
+for i in range(N-1,-1,-1):
+    cnt=0
+    for j in range(i,N,i+1): #i番目以降のiの倍数の箱にボールが何個入っているかを数える
+        if j==i:
+            continue
+        else:
+            if flag[j]==1:
+                cnt+=1
+    if (A[i]+cnt)%2==0: #箱にボールを入れるかの判定
+        flag[i]=0
+    else:
+        flag[i]=1
+        ans.append(i+1) #ボールを入れた箱のインデックスを答えに追加
+num=len(ans)
+if num==0: #ボールを入れた箱がなければ答えは0
+    print(0)
+else: #ボールを入れた箱があれば入れた箱の数とそのインデックスを出力
+    print(num)
+    print(*ans)
