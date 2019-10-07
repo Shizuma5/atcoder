080b78016f5246a8fa7b812354bdbfaeb9467c8e
Shizuma <kirua515@gmail.com>
2019-10-07 18:05:51 +0900

    A問題 AC

--- beginner_contests/con_133/a.py
+++ beginner_contests/con_133/a.py
@@ -1,5 +1,15 @@
 import sys
+
 def input():
     return sys.stdin.readline()[:-1]
 
 N, A, B = map(int, input().split())
+
+taxi = N * A
+train = B
+
+if taxi > train:
+    print(train)
+else:
+    print(taxi)
+
