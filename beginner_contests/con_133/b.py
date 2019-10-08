1eed190c211538278142ebd42532f3da6605992a
Shizuma <kirua515@gmail.com>
2019-10-09 01:19:49 +0900

    B問題 AC

--- beginner_contests/con_133/b.py
+++ beginner_contests/con_133/b.py
@@ -1,5 +1,27 @@
 import sys
+import math
 def input():
     return sys.stdin.readline()[:-1]
 
-A, B = map(int, input().split())
+N, D = map(int, input().split())
+
+X = []
+
+for n in range(N):
+    X.append(list(map(int, input().split())))
+
+def nolm(x, y):
+    s = len(x)
+    total = 0
+    for i in range(s):
+        total += (x[i] - y[i]) ** 2
+    return math.sqrt(total)
+
+ans = 0
+
+for n in range(N-1):
+    for i in range(n+1, N):
+        if nolm(X[n], X[i]).is_integer():
+            ans += 1
+
+print(ans)
