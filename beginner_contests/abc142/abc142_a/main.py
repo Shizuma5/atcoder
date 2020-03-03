47c4399f5567d7ff24a3c80eff65a847a73e04e0
Shizuma <kirua515@gmail.com>
2020-03-03 18:38:49 +0900

    a AC

--- beginner_contests/abc142/abc142_a/main.py
+++ beginner_contests/abc142/abc142_a/main.py
@@ -1,3 +1,11 @@
-# Code for A - Odds of Oddness
-# Use input() to fetch data from STDIN
-print("Hello world")
+import math
+import sys
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+N = int(input())
+
+odd = math.ceil(N / 2)
+
+print(odd / N)
