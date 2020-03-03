d4946bdd69a45e95fb15fe05fcd88763569df1b0
Shizuma <kirua515@gmail.com>
2020-03-03 19:57:58 +0900

    b AC

--- beginner_contests/abc142/abc142_b/main.py
+++ beginner_contests/abc142/abc142_b/main.py
@@ -1,3 +1,11 @@
-# Code for B - Roller Coaster
-# Use input() to fetch data from STDIN
-print("Hello world")
+import math
+import sys
+import numpy as np
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+N, K = map(int, input().split())
+h = np.array(list(map(int, input().split())))
+
+print(np.sum(h >= K))
