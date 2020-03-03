34aeabbea5c8cf97417a4280ca0d39596d51464f
Shizuma <kirua515@gmail.com>
2020-03-03 21:21:05 +0900

    13がwa

--- beginner_contests/abc142/abc142_d/main.py
+++ beginner_contests/abc142/abc142_d/main.py
@@ -1,28 +1,34 @@
 import numpy as np
 import copy as cp
 
-def common_divisors(m, n):
-    divisors = []
-    for i in range(1, int(n**0.5)+1):
-        if m % i == 0 and n % i == 0:
-            divisors.append(i)
-            if i != m // i and i != n // i:
-                divisors.append(n//i)
-    return divisors
+# 参考: https://qiita.com/snow67675476/items/e87ddb9285e27ea555f8
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append(i)
+
+    if temp!=1:
+        arr.append(temp)
+
+    if arr==[]:
+        arr.append(n)
+
+    return arr
+
 
 A, B = map(int, input().split())
 
-divisors = np.array(sorted(common_divisors(A, B)))
+ans = [1]
 
-ans = cp.deepcopy(divisors)
+for a in factorization(A):
+    for b in factorization(B):
+        if a == b:
+            ans.append(a)
 
-no_ans = []
-
-for d in divisors:
-    if d in no_ans or d == 1:
-        continue
-    ans = ans[ans % d != 0]
-    no_ans += list(divisors[divisors % d == 0])
-    ans = np.append(ans, d)
-    ans.sort()
 print(len(ans))
