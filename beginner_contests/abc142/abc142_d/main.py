0db0876ae08944e032f2119ee4db0f8fd288c88c
Shizuma <kirua515@gmail.com>
2020-03-03 21:11:02 +0900

    testcase1, 2がWA

--- beginner_contests/abc142/abc142_d/main.py
+++ beginner_contests/abc142/abc142_d/main.py
@@ -1,3 +1,28 @@
-# Code for D - Disjoint Set of Common Divisors
-# Use input() to fetch data from STDIN
-print("Hello world")
+import numpy as np
+import copy as cp
+
+def common_divisors(m, n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if m % i == 0 and n % i == 0:
+            divisors.append(i)
+            if i != m // i and i != n // i:
+                divisors.append(n//i)
+    return divisors
+
+A, B = map(int, input().split())
+
+divisors = np.array(sorted(common_divisors(A, B)))
+
+ans = cp.deepcopy(divisors)
+
+no_ans = []
+
+for d in divisors:
+    if d in no_ans or d == 1:
+        continue
+    ans = ans[ans % d != 0]
+    no_ans += list(divisors[divisors % d == 0])
+    ans = np.append(ans, d)
+    ans.sort()
+print(len(ans))
