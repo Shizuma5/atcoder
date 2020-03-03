825a4114d9821d4ce4cecef8baef072b389d30a1
Shizuma <kirua515@gmail.com>
2020-03-03 20:14:39 +0900

    C AC

--- beginner_contests/abc142/abc142_c/main.py
+++ beginner_contests/abc142/abc142_c/main.py
@@ -1,3 +1,18 @@
-# Code for C - Go to School
-# Use input() to fetch data from STDIN
-print("Hello world")
+import math
+import sys
+import numpy as np
+
+def input():
+    return sys.stdin.readline()[:-1]
+
+N = int(input())
+A = np.array(list(map(int, input().split())))
+
+ans = [0] * (N+1)
+
+for i, a in enumerate(A):
+    ans[a] = i+1
+ans.pop(0)
+
+
+print(*ans)
