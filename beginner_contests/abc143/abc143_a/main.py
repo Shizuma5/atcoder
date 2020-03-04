e2fd073f33d42abd118761c43cfd3a1bed7ac871
Shizuma <kirua515@gmail.com>
2020-03-04 10:11:28 +0900

    A AC

--- beginner_contests/abc143/abc143_a/main.py
+++ beginner_contests/abc143/abc143_a/main.py
@@ -1,3 +1,6 @@
-# Code for A - Curtain
-# Use input() to fetch data from STDIN
-print("Hello world")
+A, B = map(int, input().split())
+
+if A - (2 * B) <= 0:
+    print(0)
+else:
+    print(A - (2 * B))
