c8faadddea68a6a5f4eaa4b09dd77002eba35da9
Shizuma <kirua515@gmail.com>
2020-03-04 11:21:38 +0900

    TLE

--- beginner_contests/abc143/abc143_d/main.py
+++ beginner_contests/abc143/abc143_d/main.py
@@ -1,3 +1,12 @@
-# Code for D - Triangles
-# Use input() to fetch data from STDIN
-print("Hello world")
+N = int(input())
+L = list(map(int, input().split()))
+
+ans = 0
+
+for a in range(N):
+    for b in range(a+1, N):
+        for c in range(b+1, N):
+            if L[a] < L[b] + L[c] and L[b] < L[c] + L[a] and L[c] < L[a] + L[b]:
+                ans += 1
+
+print(ans)
