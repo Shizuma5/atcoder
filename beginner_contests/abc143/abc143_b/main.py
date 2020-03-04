880cebeae4b62d5a003482084f94802e8e6279b1
Shizuma <kirua515@gmail.com>
2020-03-04 10:35:04 +0900

    B AC

--- beginner_contests/abc143/abc143_b/main.py
+++ beginner_contests/abc143/abc143_b/main.py
@@ -1,3 +1,12 @@
-# Code for B - TAKOYAKI FESTIVAL 2019
-# Use input() to fetch data from STDIN
-print("Hello world")
+N = int(input())
+D = list(map(int, input().split()))
+
+ans = 0
+
+for n in range(N):
+    for i, d in enumerate(D):
+        if i == n:
+            continue
+        else:
+            ans += d * D[n]
+print(int(ans/2))
