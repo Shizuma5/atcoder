e0895383f4aa1894f730b555ecaa11d9fe881ba6
Shizuma <kirua515@gmail.com>
2020-06-06 23:00:36 +0900

    c AC

--- beginner_contests/abc144/abc144_c/main.py
+++ beginner_contests/abc144/abc144_c/main.py
@@ -1,3 +1,24 @@
-# Code for C - Walk on Multiplication Table
-# Use input() to fetch data from STDIN
-print("Hello world")
+import fractions
+
+def make_divisor_list(num):
+    i = 1
+    table = []
+    while i * i <= num:
+        if num%i == 0:
+            table.append(i)
+            table.append(num//i)
+        i += 1
+    table = list(set(table))
+    return table
+
+N = int(input())
+
+divisors = sorted(make_divisor_list(N))
+center = int(len(divisors) / 2)
+
+if len(divisors) % 2 == 1:
+    ans = (divisors[center] - 1) * 2
+else:
+    ans = divisors[center] - 1 + divisors[center-1] - 1
+
+print(ans)
