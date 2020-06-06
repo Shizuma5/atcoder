0ce44414e29876bfa51bce836229b94b3dad12e9
Shizuma <kirua515@gmail.com>
2020-06-06 22:27:01 +0900

    b AC

--- beginner_contests/abc144/abc144_b/main.py
+++ beginner_contests/abc144/abc144_b/main.py
@@ -1,3 +1,8 @@
-# Code for B - 81
-# Use input() to fetch data from STDIN
-print("Hello world")
+N = int(input())
+
+match_list = list(set([i * j for i in range(1, 10) for j in range(1, 10)]))
+
+if N in match_list:
+    print('Yes')
+else:
+    print('No')
