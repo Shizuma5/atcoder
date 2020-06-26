14395843a684bd5d661bbfdb6c34b86584dab373
Shizuma <kirua515@gmail.com>
2020-06-26 16:23:50 +0900

    b AC

--- beginner_contests/abc145/abc145_b/main.py
+++ beginner_contests/abc145/abc145_b/main.py
@@ -1,3 +1,13 @@
-# Code for B - Echo
-# Use input() to fetch data from STDIN
-print("Hello world")
+N = int(input())
+S = input()
+
+if N % 2 == 1:
+    print('No')
+else:
+    N = int(N / 2)
+    T1 = S[:N]
+    T2 = S[N:]
+    if T1 == T2:
+        print('Yes')
+    else:
+        print('No')
