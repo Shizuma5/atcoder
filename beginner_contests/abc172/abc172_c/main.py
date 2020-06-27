a707b7a59227ecb8873f3b728cd4ddba4f7fceee
Shizuma <kirua515@gmail.com>
2020-06-27 22:56:23 +0900

    c d 共にダメ

--- beginner_contests/abc172/abc172_c/main.py
+++ beginner_contests/abc172/abc172_c/main.py
@@ -1,3 +1,32 @@
-# Code for C - Tsundoku
-# Use input() to fetch data from STDIN
-print("Hello world")
+N, M, K = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# 経過時間
+t = 0
+# 読んだ本
+book_count = 0
+
+while t <= K:
+    if len(A) == 0 and len(B) == 0:
+        break
+    elif len(A) == 0:
+        t += B.pop(0)
+        if t <= K:
+            book_count += 1
+    elif len(B) == 0:
+        t += A.pop(0)
+        if t <= K:
+            book_count += 1
+    else:
+        if A[0] < B[0]:
+            t += A.pop(0)
+            if t <= K:
+                book_count += 1
+        else:
+            t += B.pop(0)
+            if t <= K:
+                book_count += 1
+
+print(book_count)
--- beginner_contests/abc172/abc172_d/main.py
+++ beginner_contests/abc172/abc172_d/main.py
@@ -1,3 +1,18 @@
-# Code for D - Sum of Divisors
-# Use input() to fetch data from STDIN
-print("Hello world")
+def make_divisors(n):
+    lower_divisors , upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+N = int(input())
+ans = 1
+
+for k in range(2, N+1):
+    ans += k * len(make_divisors(k))
+
+print(ans)
